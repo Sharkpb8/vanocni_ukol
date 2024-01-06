a34dc0675d58f198660dbf92031e8287e392c05a
Sharkpb8 <sharkpb8@gmail.com>
2024-01-06 15:33:23 +0100

    random color for circle

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function generate_color(){
     var letters = "0123456789ABCDEF"; 
-    var color = '#'; 
+    var color = '#';
+    var classid = ".circle"; 
     for(let i = 0; i<6;i++){
         color += letters[(Math.floor(Math.random() * 16))]; 
     }
-    document.getElementById("rc").innerHTML = color;   
+    document.getElementById("rc").innerHTML = color;
+    /* for(let i = 0;i<9;i++){
+        let idincrease = classid+i;
+        console.log(idincrease);
+        document.querySelector(idincrease).style["background-color"]= color;
+    } */
+
+    document.querySelector(".circle1").style["background-color"]= color;
+    
 }
