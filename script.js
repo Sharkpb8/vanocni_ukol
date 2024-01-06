ce87f5076221e75f8c5306dbcced209d99009aae
Sharkpb8 <sharkpb8@gmail.com>
2024-01-06 15:56:53 +0100

    circle rn color+set ammount

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function generate_color(){
     var letters = "0123456789ABCDEF"; 
     var color = '#';
-    var classid = ".circle"; 
-    for(let i = 0; i<6;i++){
+    var classid = ".circle";
+    var ammount =  document.getElementById("numbeOfCircles").value;
+    /* for(let i = 0; i<6;i++){
         color += letters[(Math.floor(Math.random() * 16))]; 
-    }
-    document.getElementById("rc").innerHTML = color;
-    /* for(let i = 0;i<9;i++){
-        let idincrease = classid+i;
-        console.log(idincrease);
-        document.querySelector(idincrease).style["background-color"]= color;
     } */
+    
+    for(let i = 1;i<=ammount;i++){
+        var idIncrease = classid+String(i);
+        for(let i = 0; i<6;i++){
+            color += letters[(Math.floor(Math.random() * 16))]; 
+        }
+        console.log(idIncrease);
+        console.log(color);
+        document.querySelector(idIncrease).style["background-color"]= color;
+        color = "#";
+    }
 
     document.querySelector(".circle1").style["background-color"]= color;
     
