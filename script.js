1f95089591ac04f35021f86f4031d12648428f90
Sharkpb8 <sharkpb8@gmail.com>
2024-01-06 17:44:10 +0100

    check for winner

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var ammount;
+const colorList = [];
+var winner;
 
 function generate_color(){
     var letters = "0123456789ABCDEF"; 
     var color = '#';
     var classid = ".circle";
-    const colorList = [];
     
     for(let i = 1;i<=ammount;i++){
         var idIncrease = classid+String(i);
@@ -18,9 +19,10 @@ function generate_color(){
         color = "#";
     }
     var random = Math.floor(Math.random()* ammount);
+    winner = colorList[random]
     console.log(random);
-    console.log(colorList[random]);
-    document.getElementById("gc").innerHTML = colorList[random];
+    console.log(winner);
+    document.getElementById("gc").innerHTML = "What circle is "+ winner;
 
 }
 
@@ -58,4 +60,94 @@ function ammountpicker9(){
 
 function ammountpicker10(){
     ammount = 10;
+}
+
+function check1(){
+    if(colorList[0]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check2(){
+    if(colorList[1]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check3(){
+    if(colorList[2]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check4(){
+    if(colorList[3]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check5(){
+    if(colorList[4]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check6(){
+    if(colorList[5]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check7(){
+    if(colorList[6]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check8(){
+    if(colorList[7]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check9(){
+    if(colorList[8]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
+}
+
+function check10(){
+    if(colorList[9]==winner){
+        document.getElementById("an").innerHTML = "correct";
+        generate_color();
+    }else{
+        document.getElementById("an").innerHTML = "Wrong";
+    }
 }
