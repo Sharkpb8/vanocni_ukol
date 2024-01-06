9a29aa5ebb7e9c65ddeddee62515a381bee04bc2
Sharkpb8 <sharkpb8@gmail.com>
2024-01-06 16:25:19 +0100

    ammount by onclick

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
+var ammount;
+
 function generate_color(){
     var letters = "0123456789ABCDEF"; 
     var color = '#';
     var classid = ".circle";
-    var ammount =  document.getElementById("numbeOfCircles").value;
-    /* for(let i = 0; i<6;i++){
-        color += letters[(Math.floor(Math.random() * 16))]; 
-    } */
     
     for(let i = 1;i<=ammount;i++){
         var idIncrease = classid+String(i);
@@ -17,7 +15,40 @@ function generate_color(){
         document.querySelector(idIncrease).style["background-color"]= color;
         color = "#";
     }
+}
 
-    document.querySelector(".circle1").style["background-color"]= color;
-    
+function ammountpicker2(){
+    ammount = 2;
+}
+
+function ammountpicker3(){
+    ammount = 3;
+}
+
+function ammountpicker4(){
+    ammount = 4;
+}
+
+function ammountpicker5(){
+    ammount = 5;
+}
+
+function ammountpicker6(){
+    ammount = 6;
+}
+
+function ammountpicker7(){
+    ammount = 7;
+}
+
+function ammountpicker8(){
+    ammount = 8;
+}
+
+function ammountpicker9(){
+    ammount = 9;
+}
+
+function ammountpicker10(){
+    ammount = 10;
 }
